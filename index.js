4b72c63593aece36ca74c725263bbe87f72cf3ab
Anar-Qandayev <tu7bxfga8@code.edu.az>
2023-04-25 00:53:24 +0400

    first commit

---
+++ index.js
@@ -0,0 +1,44 @@
+// !           Task 1
+
+// function printNumbers(){
+//     for(let i=0;i<10;i++){
+//         console.log(i)
+//     }
+// }
+// printNumbers();
+
+// !          Task 2
+
+// let array=[[2,3,4],[6,7,8,8]];
+// function printEven(){
+//     for (let i = 0; i <= array.length; i++) {
+//      var arr = array[i];
+//      for (let j = 0; j <= arr.length; j++) {
+//        if (array[i][j] % 2 == 0) {
+//          console.log(array[i][j]);
+//        }
+//      }
+//     }
+
+//     for (let i of array) {
+//      for (let j of i) {
+//        if (j % 2 == 0) {
+//          console.log(j);
+//        }
+//      }
+//     }
+// }
+// printEven();
+
+// !     Task 3
+
+// function printPattern(){
+//     for(let i=0;i<=5;i++){
+//         let s = "";
+//         for(let j=1;j<=i;j++){
+//             s += j;
+//         }
+//         console.log(s)
+//     }
+// }
+// printPattern();
